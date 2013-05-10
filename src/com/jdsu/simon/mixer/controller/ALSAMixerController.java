b72383784432157d3f9f17aac0189935ae3a5145
SiMing Weng <siming.weng@gmail.com>
2013-05-10 22:32:46 +0800

    add convenient method

--- src/com/jdsu/simon/mixer/controller/ALSAMixerController.java
+++ src/com/jdsu/simon/mixer/controller/ALSAMixerController.java
@@ -18,6 +18,10 @@ public class ALSAMixerController {
     public ALSAMixerController() {
     }
     
+    public void setInCallMusicDelivery(boolean on){
+        controllMixer("", on ? 1 : 0);
+    }
+    
     public native int controllMixer(String mixerName, int value);
 
 }
