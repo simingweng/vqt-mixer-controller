8894a24dedf105e65adf88bcc573dd90ac7cbe6f
SiMing Weng <siming.weng@gmail.com>
2013-05-13 00:40:57 +0800

    implement the JNI wrapper function to call amix export functionality

--- jni/amixwrapper.c
+++ jni/amixwrapper.c
@@ -1,10 +1,26 @@
+#include <stdio.h>
+
 #include "com_jdsu_simon_mixer_controller_ALSAMixerController.h"
 #include "amix_export.h"
 
 JNIEXPORT jint JNICALL Java_com_jdsu_simon_mixer_controller_ALSAMixerController_controllMixer
   (JNIEnv *env, jobject obj, jobjectArray args){
-	char *argv[2];
-	argv[0] = "mixer_control_name";
-	argv[1] = "1";
-	return configure_mixer(2, argv);
+	int length = (*env)->GetArrayLength(env, args);
+	char *argv[length];
+	int i = 0;
+	for(i=0 ; i++ ; i<length){
+		jstring arg = (jstring)(*env)->GetObjectArrayElement(env, args, i);
+		char *carg = (*env)->GetStringUTFChars(env, arg, NULL);
+		if(carg != NULL){
+			argv[i] = carg;
+			printf("%s", argv[i]);
+		}
+	}
+	int result = -1;
+	result = configure_mixer(length, argv);
+	for(i=0 ; i++; i<length){
+		jstring arg = (jstring)(*env)->GetObjectArrayElement(env, args, i);
+		(*env)->ReleaseStringUTFChars(env, arg, argv[i]);
+	}
+	return result;
 }
